e2b00fd2cfc2a928ff00c3002733542d786d1857
PratibhaR <demoemail@gmail.com>
2024-01-05 10:25:25 +0530

    Add project files.

---
+++ Jan05_WebCore_JenkinsDemo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Jan05_WebCore_JenkinsDemo.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
